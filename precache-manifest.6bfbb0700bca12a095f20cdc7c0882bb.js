aa870b40e957b8d325b8f5d03483b9921e3138d9
Ajay <meditatewithzen@gmail.com>
2019-02-23 18:16:52 +0530

    Updates

---
+++ precache-manifest.6bfbb0700bca12a095f20cdc7c0882bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a350caa1230c12ced9f",
+    "url": "/reactContactManager/static/js/runtime~main.8a350caa.js"
+  },
+  {
+    "revision": "3ee38e08a5c5ae338d4b",
+    "url": "/reactContactManager/static/js/main.3ee38e08.chunk.js"
+  },
+  {
+    "revision": "85252195c2d87ae667af",
+    "url": "/reactContactManager/static/js/2.85252195.chunk.js"
+  },
+  {
+    "revision": "3ee38e08a5c5ae338d4b",
+    "url": "/reactContactManager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "85252195c2d87ae667af",
+    "url": "/reactContactManager/static/css/2.99409602.chunk.css"
+  },
+  {
+    "revision": "abf849cb93d7019674d6ba2d24a4f1ac",
+    "url": "/reactContactManager/index.html"
+  }
+];
